87673d6b7ad286b202e2ed3fa087c70888d4eca5
ankush <ankushjadevv4tech@gmail.com>
2024-10-07 18:13:57 +0530

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create!(
+  name: "HR Manager",
+  email: "hr@example.com",
+  password: "password123",
+  password_confirmation: "password123",
+  role: :hr
+)
+
+User.create!(
+  name: "John Doe",
+  email: "employee@example.com",
+  password: "password123",
+  password_confirmation: "password123",
+  role: :employee
+)
